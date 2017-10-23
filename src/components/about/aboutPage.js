4bc25699caadea91a95a32cd136e5778cc2269b4
Raney Eden <raneyeden@hotmail.com>
2017-10-23 09:16:19 -0400

    Added reusable text input component

--- src/components/about/aboutPage.js
+++ src/components/about/aboutPage.js
@@ -3,6 +3,21 @@
 var React = require('react');
 
 var About = React.createClass({
+	statics: {
+		willTransitionTo: function(transition, params, query, callback) {
+			if (!confirm('Are you sure you want read a page that\'s this boring?')) {
+				transition.abort();
+			} else {
+				callback();
+			}
+		},
+		willTransitionFrom: function(transition, component) {
+			if (!confirm('Are you sure you want leave a page that\'s this exciting?')) {
+				transition.abort();
+			}
+		}	
+	},
+
 	render: function () {
 		return (
 			<div>
--- src/routes.js
+++ src/routes.js
@@ -13,6 +13,7 @@ var routes = (
         <DefaultRoute handler={require('./components/homePage')} />
         <Route name="authors" handler={require('./components/authors/authorPage')} />
         <Route name="about" handler={require('./components/about/aboutPage')} />
+        <Route name="addAuthor" path="author" handler={require('./components/authors/manageAuthorPage')} />
         <NotFoundRoute handler={require('./components/common/notFoundPage')} />
         <Redirect from="about*" to="about" />
         <Redirect from="awthurs" to="authors" />
